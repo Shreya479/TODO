99b9d2d89c7f7b8f9da0b5f20b221a4226473bb7
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-29 00:13:19 +0530

    Add files via upload

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'shreya', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'shreya', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'shreya', 'Learn Microservices', sysdate(), false);
