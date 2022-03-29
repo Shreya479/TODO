2b8851ef97d1acc8618550bffd93711f4a645379
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-29 12:19:58 +0530

    Update ErrorComponent.jsx

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function ErrorComponent() {
-    return <div>An Error Occurred. I don't know what to do! Contact support at abcd-efgh-ijkl</div>
+    return <div>An Error Occurred. I don't know what to do! Contact support at 123-567-8901</div>
 }
 
 export default ErrorComponent
